e2ba2732c85091e18312b258d84cb63e64326341
jhon <jhonmarbenvaides2728@gmail.com>
2025-08-09 10:41:01 -0500

    subiendo archivos

---
+++ nicole.js
@@ -0,0 +1,10 @@
+var numero=13;
+console.log("el valor del numero:"+ numero);
+let nombre="Nicole García Mosquera"
+var edad=17;
+var salario=1000000;
+var hijos=3;
+console.log("mi nombre es:"+nombre+ " , mi edad es:" +edad+ ", mi sueldo es:" +salario+" y tendre tres hijos: "+hijos);
+let masculine= false;
+let femenino= true;
+console.log("mi genero es"+ " femenino"+ femenino );
