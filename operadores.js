5dacc5fd407bcaec970ec15e4b9ce2d0787e3989
jhon <jhonmarbenvaides2728@gmail.com>
2025-08-09 09:48:42 -0500

    aa

---
+++ operadores.js
@@ -0,0 +1,34 @@
+// suma, resta, multiplicacion y division
+// calcular valores de do o mas elementos
+let num1 = 4;
+var num2 = 8;
+let suma = num1 + num2;
+console.log("el resultado es: " + suma)
+//contadores
+let edad = 17;
+edad=edad+1 //edad++
+console.log("edad es: " + edad)
+//resta
+let rest1 = 7;
+var rest2 = 5;
+let resta = rest1 - rest2;
+console.log("el resultado  es: " + resta)
+//contadores
+let edad1 = 17;
+edad1=edad1-1 //años--
+console.log("edad1 es: " + edad1)
+//multiplicacion
+let mul1 = 10;
+let mul2 = 6;
+let multiplicacion = mul1 * mul2;
+console.log("el resultado es: " + multiplicacion)
+//division
+let div1 = 6
+let div2 = 2
+let division = div1/div2;
+console.log("el resultado es: " + division)
+//residuo
+let res1 =17;
+let res2 =4;
+let residuo =res1%res2;
+console.log("el residuo es: " + residuo);
